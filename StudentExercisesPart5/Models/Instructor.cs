bf171281a6326f7e9274e70a9d5e5014665f62df
unknown <asiamcarter@gmail.com>
2019-03-23 11:06:03 -0500

    first commit

---
+++ StudentExercisesPart5/Models/Instructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentExercises_v604.Model
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SlackHandle { get; set; }
+        public int CohortId { get; set; }
+        public Cohort cohort { get; set; }
+    }
+}
---
+++ StudentExercisesPart5/Models/StudentExercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentExercises_v604.Model
+{
+    class StudentExercise
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+    }
+}
