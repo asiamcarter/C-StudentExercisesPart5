e77060faf9d3cb77588ae4031fa2c4480aca478f
unknown <asiamcarter@gmail.com>
2019-03-27 13:15:13 -0500

    student exercise part 7 complete

--- StudentExercisesPart5/Models/Student.cs
+++ StudentExercisesPart5/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using SEWebApi.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SEWebApi.Model
@@ -10,6 +11,7 @@ namespace SEWebApi.Model
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [StringLength(12, MinimumLength = 3)]
         public string SlackHandle { get; set; }
         public int CohortId { get; set; }
         public Cohort cohort { get; set; }
